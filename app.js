ea6880912bc7c58bfafa0407a9b25111f868c569
Mr.AK <akhilsaivaraganti@gmail.com>
2021-03-24 19:47:51 +0530

    refactored the code

--- app.js
+++ app.js
@@ -1,57 +1,42 @@
 let body = document.querySelector("body");
 let svg = document.querySelector("#svg");
 let mathsLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
-let phyLine = document.createElementNS("http://www.w3.org/2000/svg", "line")
-let chemLine = document.createElementNS("http://www.w3.org/2000/svg", "line")
-let maths = document.querySelector("#maths").value;
-let physics = document.querySelector("#physics").value;
-let chemistry = document.querySelector("#chemistry").value;
+let phyLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
+let chemLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
+let maths = document.querySelector("#maths");
+let physics = document.querySelector("#physics");
+let chemistry = document.querySelector("#chemistry");
 
 let x1 = 0;
 let y1 = 100;
 let x2 = 0;
 let y2 = 0;
 
-let mathsValue = Number(maths);
-let physicsValue = Number(physics);
-let chemistryValue = Number(chemistry);
-
 function drawGraph() {
-        y2 =y2 + (101 -  mathsValue);
-        x2 = 30;
-        mathsLine.setAttribute("x1", x1);
-        mathsLine.setAttribute("y1", y1);
-        mathsLine.setAttribute("x2", x2);
-        mathsLine.setAttribute("y2", y2);
-        mathsLine.setAttribute("stroke", "red");
-        svg.append(mathsLine);
-        x1 = x2;
-        y1 = y2;
-        physicsGraph(x1,y1);
+  let subjects = [];
+  subjects.push(maths.value, physics.value, chemistry.value);
+  let lines = [];
+  lines.push(mathsLine, phyLine, chemLine);
+  let colors = [];
+  colors.push("red", "green", "blue");
+
+  for (let i = 0; i < subjects.length; i++) {
+    y2 = 101 - Number(subjects[i]);
+    x2 = x2 + 30;
+    lines[i].setAttribute("x1", x1);
+    lines[i].setAttribute("y1", y1);
+    lines[i].setAttribute("x2", x2);
+    lines[i].setAttribute("y2", y2);
+    lines[i].setAttribute("stroke", colors[i]);
+    svg.append(lines[i]);
+    x1 = x2;
+    y1 = y2;
+    emptyingValues();
+  }
 }
-function physicsGraph(x1,y1){
-        y2 = 0;
-        y2 =y2 + (101 - ( physicsValue));
-        x2 = 60;
-        phyLine.setAttribute("x1", x1);
-        phyLine.setAttribute("y1", y1);
-        phyLine.setAttribute("x2", x2);
-        phyLine.setAttribute("y2", y2);
-        phyLine.setAttribute("stroke", "green");
-        svg.append(phyLine);
-        x1 = x2;
-        y1 = y2;
-        chemistryGraph(x1,y1);
-}
-function chemistryGraph(x1,y1){
-        y2 = 0;
-        y2 =y2 + (101 - ( chemistryValue));
-        x2 = 90;
-        chemLine.setAttribute("x1", x1);
-        chemLine.setAttribute("y1", y1);
-        chemLine.setAttribute("x2", x2);
-        chemLine.setAttribute("y2", y2);
-        chemLine.setAttribute("stroke", "blue");
-        svg.append(chemLine);
-        maths = "";
+
+function emptyingValues() {
+  maths.value = "";
+  physics.value = "";
+  chemistry.value = "";
 }

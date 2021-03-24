b9e543590d45f91389124127b29c4acf68f37cfd
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-03-24 16:57:21 +0530

    second commit

--- app.js
+++ app.js
@@ -1,24 +1,58 @@
 let body = document.querySelector("body");
 let svg = document.querySelector("#svg");
-let line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-let maths = document.querySelector("#maths");
-let physics = document.querySelector("#physics");
+let mathsLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
+let phyLine = document.createElementNS("http://www.w3.org/2000/svg", "line")
+let chemLine = document.createElementNS("http://www.w3.org/2000/svg", "line")
+let maths = document.querySelector("#maths").value;
+let physics = document.querySelector("#physics").value;
 let chemistry = document.querySelector("#chemistry").value;
 
-let x1 = 10;
-let y1 = 90;
+let x1 = 0;
+let y1 = 100;
 let x2 = 0;
 let y2 = 0;
 
-function drawGraph() {
-        line.setAttribute("x1", x1);
-        line.setAttribute("y1", y1);
-        line.setAttribute("x2", maths.value);
-        line.setAttribute("y2", -maths.value);
-        line.setAttribute("stroke", "red");
-        svg.append(line);
+let mathsValue = Number(maths);
+let physicsValue = Number(physics);
+let chemistryValue = Number(chemistry);
 
-        console.log(-maths.value); 
-        maths.value = "";
-        physics.value = "";
+function drawGraph() {
+        y2 =y2 + (100 -  mathsValue);
+        x2 = 30;
+        mathsLine.setAttribute("x1", x1);
+        mathsLine.setAttribute("y1", y1);
+        mathsLine.setAttribute("x2", x2);
+        mathsLine.setAttribute("y2", y2);
+        mathsLine.setAttribute("stroke", "red");
+        svg.append(mathsLine);
+        x1 = x2;
+        y1 = y2;
+        physicsGraph(x1,y1);
+        // console.log(x1,x2,y1,y2);
+}
+function physicsGraph(x1,y1){
+        y2 = 0;
+        y2 =y2 + (100 - ( physicsValue));
+        x2 = 60;
+        phyLine.setAttribute("x1", x1);
+        phyLine.setAttribute("y1", y1);
+        phyLine.setAttribute("x2", x2);
+        phyLine.setAttribute("y2", y2);
+        phyLine.setAttribute("stroke", "green");
+        svg.append(phyLine);
+        x1 = x2;
+        y1 = y2;
+        chemistryGraph(x1,y1);
+}
+function chemistryGraph(x1,y1){
+        y2 = 0;
+        y2 =y2 + (100 - ( chemistryValue));
+        x2 = 90;
+        chemLine.setAttribute("x1", x1);
+        chemLine.setAttribute("y1", y1);
+        chemLine.setAttribute("x2", x2);
+        chemLine.setAttribute("y2", y2);
+        chemLine.setAttribute("stroke", "blue");
+        svg.append(chemLine);
+        maths = "";
 }
